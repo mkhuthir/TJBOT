b3a5f4296d32213ce9e2b24fb876f8bcbe604c88
Muthanna A. Attyah <mkhuthir@gmail.com>
2019-03-25 15:04:00 +0400

    Update lab2_shine_led.js

--- labs/lab2_shine_led.js
+++ labs/lab2_shine_led.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
-/* Lab 2: Shine and pulse the LED light - Shine LED */
+/* Lab 2: Shine and pulse the LED light - Shine LED 
+
+TJBot has an LED that can be controlled to signal the user about various conditions using a color-coded scheme.
+
+*/
 
 var TJBot = require("tjbot");
 
