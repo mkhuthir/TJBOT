5ae943e897d2f9c46f861a999676efb2113a000a
Muthanna A. Attyah <mkhuthir@gmail.com>
2019-03-25 15:08:43 +0400

    Update lab4_stt.js

--- labs/lab4_stt.js
+++ labs/lab4_stt.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node    
 
-/* Lab 4: Convert speech to text */
+/* Lab 4: Convert speech to text 
+
+TJBot can be trained to listen to natural phrases spoken into the microphone. Several types of microphones are available, 
+each with varying degrees of audio quality. In practice, some work much better in noisy environments. 
+Others do better when pointed directly at the source of the audio. USB microphones are the easiest to use where you can plug and play.
+The TJBot kit comes with a small stubby USB microphone. The microphone should face toward the front of the robot.
+
+Depending on the way the Raspberry Pi detects the microphone, you might need to instruct the TJBot library to use a different
+speaker device ID.
+
+*/
 
 var TJBot = require("tjbot");
 
