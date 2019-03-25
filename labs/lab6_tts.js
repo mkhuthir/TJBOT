0b2d0e194e52143ae4f4fc023802e4f7860aca98
Muthanna A. Attyah <mkhuthir@gmail.com>
2019-03-25 14:59:21 +0400

    Update lab6_tts.js

--- labs/lab6_tts.js
+++ labs/lab6_tts.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node    
 
-/* Lab 6: Make the robot speak */
+/* Lab 6: Make the robot speak 
+
+TJBot can be trained to speak phrases aloud in 13 voices in 7 languages using the Watson Text to Speech API. Just like the microphone hardware, you can use various speakers with the TJBot. See the documentation for your speaker about how to connect the speaker to the Raspberry Pi. Because of an issue with the 3.5mm audio port conflicting with the LED light, you should avoid plugging a speaker directly into the 3.5mm port. Some speakers are easier to use, including USB speakers. Using a USB speaker allows you to plug and play. You might need to locate the speaker device ID and instruct the TJBot library to use that speaker.
+
+*/
 
 var TJBot = require("tjbot");
     
