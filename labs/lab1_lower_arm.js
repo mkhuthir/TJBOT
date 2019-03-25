024f1d281134fe73e28d2269b16e3224b840900c
Muthanna A. Attyah <mkhuthir@gmail.com>
2019-03-25 15:01:51 +0400

    Update lab1_lower_arm.js

--- labs/lab1_lower_arm.js
+++ labs/lab1_lower_arm.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 
-/* Lab 1: Move the robot's arm - Lower The Arm */
+/* Lab 1: Move the robot's arm - Lower The Arm 
+
+TJBot has a servo connected to the left arm that can be controlled to signal the user about various conditions using movement.
+You can make the arm move in several ways:
+
+Lower the arm
+Raise the arm
+Move the back
+Wave the arm up and down
+
+*/
 
 var TJBot = require("tjbot");
 
