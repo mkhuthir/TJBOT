9b7ba80000fa57c665028ff839bf08292575a386
Muthanna A. Attyah <mkhuthir@gmail.com>
2019-03-25 15:28:21 +0400

    Update lab9_converse.js

--- labs/lab9_converse.js
+++ labs/lab9_converse.js
@@ -3,3 +3,48 @@
 /* Lab 9:
 
 */
+
+var TJBot = require("tjbot");
+
+var tj = new TJBot(
+  ["microphone","speaker"],
+  {
+    robot: {
+      gender: "male"
+    },
+    speak: {
+      language: "en-US"
+    }
+  },
+  {
+    speech_to_text: {
+      username: "",
+      password: ""
+    },
+    conversation: {
+      username: "",
+      password: ""
+    },
+    text_to_speech: {
+      username: "",
+      password: ""
+    }    
+  }
+);
+
+var workspaceId = "";
+
+function processText(text) {
+  console.log(text);
+  tj.stopListening();
+
+  tj.converse(workspaceId, text, function(response) {
+    console.log(response);
+
+    tj.speak(response.object.output.text.join(" ")).then(function(){
+      tj.listen(processText);
+    });
+  });
+}
+
+tj.listen(processText);
