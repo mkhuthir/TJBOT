87085953e85729f707f1ec94ad4539c1010d5771
Muthanna A. Attyah <mkhuthir@gmail.com>
2019-03-25 14:45:17 +0400

    Create lab5_translate.js

---
+++ labs/lab5_translate.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node    
+
+/* Lab 5: Translate language */
+
+
+var TJBot = require("tjbot");
+
+var tj = new TJBot(
+  [],
+  {},
+  {
+   language_translator: {    
+    apikey: ""
+   },
+  }
+);
+
+var text = "bonjour";
+
+tj.identifyLanguage(text).then(function(languages) {
+  var sourceLanguage = languages.languages[0].language;
+  var targetLanguage = "en";
+  
+  tj.isTranslatable(sourceLanguage, targetLanguage).then(function(result) {
+    if(result) {
+      tj.translate(text, sourceLanguage, targetLanguage).then(function(translation) {
+        console.log(translation.translations[0].translation);
+      });
+    } else {
+      console.log("Unable to translate from '"+sourceLanguage+"' to '"+targetLanguage+"'");
+    }
+  });  
+});
+
