b6156d4e6898f3be57862171bef743d546a4655e
Muthanna A. Attyah <mkhuthir@gmail.com>
2019-03-25 15:06:24 +0400

    Update lab3_take_photo.js

--- labs/lab3_take_photo.js
+++ labs/lab3_take_photo.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 
-/* Lab 3: Make the robot see: take photo */
+/* Lab 3: Make the robot see: take photo 
+
+TJBot can be trained to take photos and recognize objects and colors. The physical TJBot uses a Raspberry Pi camera.
+The flex cable inserts into the connector situated between the Ethernet and HDMI ports, with the silver connectors 
+facing the HDMI port.
+
+This lab requires the Watson Visual Recognition service.
+
+*/
 
 var TJBot = require("tjbot");
 
