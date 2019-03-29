de085684f4c526aec4eb732208c164dbe1695c2d
mkhuthir <mkhuthir@gmail.com>
2019-03-29 22:55:38 +0400

    Updated shell script

--- bash/stt_time_stamps.sh
+++ bash/stt_time_stamps.sh
@@ -4,7 +4,7 @@
 
 
 if test "$#" -ne 1; then
-    	echo "Usage: ./stt_time_stamps.sh \"apikey\":\"xxxxxxx\""
+    echo "Usage: ./stt_time_stamps.sh \"apikey\":\"xxxxxxx\""
 else
 	curl -X POST -u $1 --header "Content-Type: audio/flac" --data-binary @speech/audio-file.flac "https://gateway-lon.watsonplatform.net/speech-to-text/api/v1/recognize?timestamps=true&max_alternatives=3"
 fi
