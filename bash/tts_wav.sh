6818de52e5b48921cf00e7eb5770324ebfa7c5d2
mkhuthir <mkhuthir@gmail.com>
2019-03-30 22:27:46 +0400

    WIP

---
+++ bash/tts_wav.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Script will convert speech of the selected file into text
+
+if test "$#" -ne 1; then
+    echo "Usage: ./stt.sh \"apikey\":\"xxxxxxx\""
+else
+	curl -X POST -u $1 \
+		--header "Content-Type: application/json" \
+		--header "Accept: audio/wav" \
+		--data "{\"text\":\"hello world\"}" \
+		--output hello_world.wav \
+		"https://gateway-lon.watsonplatform.net/text-to-speech/api/v1/synthesize"
+fi
