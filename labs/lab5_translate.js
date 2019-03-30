9d3c20fd52dce98077823f24ea301a1dabaf5101
mkhuthir <mkhuthir@gmail.com>
2019-03-30 21:18:22 +0400

    WIP

--- labs/lab5_translate.js
+++ labs/lab5_translate.js
@@ -10,18 +10,14 @@ Watson Language Translator service.
 var TJBot = require('tjbot');
 var config = require('./config');
 
-// obtain our credentials from config.js
-var credentials = config.credentials;
-
 // these are the hardware capabilities that our TJ needs for this recipe
 var hardware = [];
 
+// obtain our credentials from config.js
+var credentials = config.credentials;
+
 // set up TJBot's configuration
-var tjConfig = {
-    log: {
-        level: 'verbose'
-    }
-};
+var tjConfig = config.tjConfig;
 
 // instantiate our TJBot!
 var tj = new TJBot(hardware, tjConfig, credentials);
