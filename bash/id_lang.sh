24a45904dcfdaa7be257eef4a9a47e3ffb0343d9
mkhuthir <mkhuthir@gmail.com>
2019-03-30 21:43:30 +0400

    added translator service

---
+++ bash/id_lang.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Script will translate from English (en) to Spanish (es)
+
+if test "$#" -ne 1; then
+    echo "Usage: ./stt.sh \"apikey\":\"xxxxxxx\""
+else
+	curl -X POST -u $1 --header "Content-Type: text/plain" --data "Language Translator translates text from one language to another" "{url}/v3/identify?version=2018-05-01"
+fi
---
+++ bash/translate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Script will translate from English (en) to Spanish (es)
+
+if test "$#" -ne 1; then
+    echo "Usage: ./stt.sh \"apikey\":\"xxxxxxx\""
+else
+    curl -X POST -u $1 --header "Content-Type: application/json" --data "{\"text\": [\"Hello, world! \", \"How are you?\"], \"model_id\":\"en-es\"}" "{url}/v3/translate?version=2018-05-01"
+fi
