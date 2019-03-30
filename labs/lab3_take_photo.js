0abf7c4fadf6558c1f9125e970d93eacd9c9e5f0
mkhuthir <mkhuthir@gmail.com>
2019-03-30 13:20:04 +0400

    Updated take photo

--- labs/lab3_take_photo.js
+++ labs/lab3_take_photo.js
@@ -14,30 +14,7 @@ const targetPath='./images/photo.jpg'
 
 var TJBot = require("tjbot");
 
-var tj = new TJBot(
-                    ["camera"],
-                    {
-                      see: {
-                              confidenceThreshold: {
-                                object: 0.5,   // only list image tags with confidence > 0.5
-                                text: 0.1     // only list text tags with confidence > 0.5
-                              },
-
-                              camera: {
-                                height: 720,
-                                width: 960,
-                                verticalFlip: false, // flips the image vertically, may need to set to 'true' if the camera is installed upside-down
-                                horizontalFlip: false // flips the image horizontally, should not need to be overridden
-                              }
-                      }
-                    },
-
-                    {
-                      visual_recognition: {
-                          apikey: "xxx__paste__key__here__xxx"
-                      }
-                    }
-                    );
+var tj = new TJBot(["camera"],{},{});
 
 tj.takePhoto(targetPath).then(function(filePath) {
 	console.log('Captured Photo Path ='+filePath);
