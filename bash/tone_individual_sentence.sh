5037594490b997cfc8fa2ff0ac7d6b8302e0c9e7
Muthanna A. Attyah <mkhuthir@gmail.com>
2019-03-31 13:06:32 +0400

    Update tone_individual_sentence.sh

--- bash/tone_individual_sentence.sh
+++ bash/tone_individual_sentence.sh
@@ -1,1 +1,15 @@
+#!/bin/sh
 
+# Synthesize text in Spanish
+
+if test "$#" -ne 1; then
+    echo "Usage: ./"+$0+"\"yourkey\""
+else
+
+    curl -X POST -u "apikey:"+$1 \
+      --header "Content-Type: application/json" \
+      --data-binary @./json/tone.json \
+      "{url}/v3/tone?version=2017-09-21"
+
+
+fi
