3e70abde19db466c9abc9f07516aee0fe1f8f700
Muthanna A. Attyah <mkhuthir@gmail.com>
2019-04-01 13:52:04 +0400

    Update visual_recognition_1.sh

--- bash/visual_recognition_1.sh
+++ bash/visual_recognition_1.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
-# Classify an image
+# Classify an image using General Model
+# The response includes the classes identified in the image from the built-in General model ("classifier_id": "default")
+# By default, responses from the Classify calls don't include classes with a score below 0.5 (50%).
 
 curl -u "apikey:"+$1 /
   "https://gateway.watsonplatform.net/visual-recognition/api/v3/classify/
