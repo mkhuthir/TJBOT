6e4fc8484c2fffd10f81675c776c7443344c23a1
Muthanna A. Attyah <mkhuthir@gmail.com>
2019-04-01 13:40:58 +0400

    Update tone_analyzer_4.sh

--- bash/tone_analyzer_4.sh
+++ bash/tone_analyzer_4.sh
@@ -9,5 +9,5 @@ else
     curl -X POST -u $1 \
         --header "Content-Type: application/json" \
         --data-binary @./json/tone-chat.json \
-        "/v3/tone_chat?version=2017-09-21"
+        "https://gateway-lon.watsonplatform.net/tone-analyzer/api/v3/tone_chat?version=2017-09-21"
 fi
