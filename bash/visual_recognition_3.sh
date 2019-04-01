07567d90043b4c8c4c1e82f5cc02b24c92280b76
Muthanna A. Attyah <mkhuthir@gmail.com>
2019-04-01 14:04:50 +0400

    Update visual_recognition_3.sh

--- bash/visual_recognition_3.sh
+++ bash/visual_recognition_3.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-curl -u "apikey:{apikey}" /
+# Detect faces in an image
+# The response provides the location of the face in the image and the estimated age range and gender for each face.
+
+curl -u "apikey:{$1}" /
   "https://gateway.watsonplatform.net/visual-recognition/api/v3/detect_faces/
   ?url=https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/Ginni_Rometty_at_the_Fortune_MPW_Summit_in_2011.jpg/
   &version=2018-03-19"
