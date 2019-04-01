1caabdeaf1a40278aa65c0150b8cb419cf27dfba
Muthanna A. Attyah <mkhuthir@gmail.com>
2019-04-01 14:03:02 +0400

    Update visual_recognition_2.sh

--- bash/visual_recognition_2.sh
+++ bash/visual_recognition_2.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-curl -u "apikey:{apikey}" /
+# Classify with the Food model
+
+curl -u "apikey:{$1}" /
     -F "classifier_ids=food" /
     "https://gateway.watsonplatform.net/visual-recognition/api/v3/classify/
     ?url=https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/fruitbowl.jpg/
