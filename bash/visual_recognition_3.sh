851ad4e0dddf7418852d33dfbaccf2c21ab8e111
mkhuthir <mkhuthir@gmail.com>
2019-04-04 06:36:36 +0400

    corrected script

--- bash/visual_recognition_3.sh
+++ bash/visual_recognition_3.sh
@@ -13,3 +13,4 @@ else
     "https://gateway.watsonplatform.net/visual-recognition/api/v3/detect_faces\
     ?url=https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/Ginni_Rometty_at_the_Fortune_MPW_Summit_in_2011.jpg\
     &version=2018-03-19"
+fi
