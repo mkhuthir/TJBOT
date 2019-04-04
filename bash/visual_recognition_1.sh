0c4e35eb0c0b1ebdc2fdae521a353f07ce6b537b
mkhuthir <mkhuthir@gmail.com>
2019-04-04 06:35:33 +0400

    corrected script

--- bash/visual_recognition_1.sh
+++ bash/visual_recognition_1.sh
@@ -14,3 +14,4 @@ else
     "https://gateway.watsonplatform.net/visual-recognition/api/v3/classify\
     ?url=https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/640px-IBM_VGA_90X8941_on_PS55.jpg\
     &version=2018-03-19"
+fi
--- bash/visual_recognition_2.sh
+++ bash/visual_recognition_2.sh
@@ -13,3 +13,4 @@ else
         "https://gateway.watsonplatform.net/visual-recognition/api/v3/classify\
         ?url=https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/fruitbowl.jpg\
         &version=2018-03-19"
+fi
