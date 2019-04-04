d5aa5be91609d5690a0c506876222f0b049fd9bf
mkhuthir <mkhuthir@gmail.com>
2019-04-04 09:00:13 +0400

    Corrected script

--- bash/language_translator_1.sh
+++ bash/language_translator_1.sh
@@ -2,10 +2,13 @@
 
 # The following will translate two phrases, "Hello, world!" and "How are you?" from English to Spanish.
 
+echo "Translate two phrases from English to Spanish"
+
 if test "$#" -ne 1; then
-    echo "Usage: ./stt.sh \"apikey\":\"xxxxxxx\""
+    echo "Usage: $0 api-key"
 else
-    curl -X POST -u $1 \
+	echo " Calling Watson Service API Using -> \"apikey:$1\""
+	curl -X POST -u "apikey:$1" \
 	--header "Content-Type: application/json" \
 	--data "{\"text\": [\"Hello, world! \", \"How are you?\"], \"model_id\":\"en-es\"}" \
 	"https://gateway-lon.watsonplatform.net/language-translator/api/v3/translate?version=2018-05-01"
--- bash/language_translator_2.sh
+++ bash/language_translator_2.sh
@@ -2,10 +2,13 @@
 
 # The following will identify the language of text
 
+echo "Identify the language of text"
+
 if test "$#" -ne 1; then
-    echo "Usage: ./stt.sh \"apikey\":\"xxxxxxx\""
+    echo "Usage: $0 api-key"
 else
-	curl -X POST -u $1 \
+	echo " Calling Watson Service API Using -> \"apikey:$1\""
+	curl -X POST -u "apikey:$1" \
 		--header "Content-Type: text/plain" \
 		--data "Language Translator translates text from one language to another" \
 		"https://gateway-lon.watsonplatform.net/language-translator/api/v3/identify?version=2018-05-01"
