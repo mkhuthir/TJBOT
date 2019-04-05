849ffdfdce151303fe64c87f05a25d66b5d466ec
mkhuthir <mkhuthir@gmail.com>
2019-04-05 22:22:50 +0400

    updated labs

--- labs/config.default.js
+++ labs/config.default.js
@@ -25,6 +25,20 @@ exports.tjConfig = {
     speak: { 
         language: "en-US"
     }
+
+    see: {
+        confidenceThreshold: {
+          object: 0.1,   // only list image tags with confidence > 0.5
+          text: 0.1     // only list text tags with confidence > 0.5
+        },
+
+        camera: {
+          height: 720,
+          width: 960,
+          verticalFlip: false, // flips the image vertically, may need to set to 'true' if the camera is installed upside-down
+          horizontalFlip: false // flips the image horizontally, should not need to be overridden
+        }
+}
 };
 
 // ---  Create the credentials object for export  ---
--- labs/lab3_see.js
+++ labs/lab3_see.js
@@ -16,22 +16,8 @@ var config = require('./config');
 // these are the hardware capabilities that our TJ needs for this recipe
 var hardware = ["camera"];
 
-// TJ Configuration
-var tjConfig = {
-                  see: {
-                          confidenceThreshold: {
-                            object: 0.1,   // only list image tags with confidence > 0.5
-                            text: 0.1     // only list text tags with confidence > 0.5
-                          },
-
-                          camera: {
-                            height: 720,
-                            width: 960,
-                            verticalFlip: false, // flips the image vertically, may need to set to 'true' if the camera is installed upside-down
-                            horizontalFlip: false // flips the image horizontally, should not need to be overridden
-                          }
-                  }
-                }
+// set up TJBot's configuration
+var tjConfig = config.tjConfig;
 
 // obtain our credentials from config.js
 var credentials = config.credentials;
