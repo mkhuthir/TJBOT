5d49da7f0ab75ea367b8790fe94327a3c0ed3750
mkhuthir <mkhuthir@gmail.com>
2019-04-13 12:10:44 +0400

    updated player

--- tests/test.sound_player.js
+++ tests/test.sound_player.js
@@ -8,8 +8,8 @@ var options = {
     gain: 10,
     debug: true,
     player: 'aplay',
-    device: 'bluealsa:HCI=hci0,DEV=11:2C:33:A4:1E:5B,PROFILE=a2dp' //  add your bluetooth device mac address
-    //device: "plughw:0,0" // plugged-in USB card 1, device 0; see aplay -l for a list of playback devices
+    //device: 'bluealsa:HCI=hci0,DEV=11:2C:33:A4:1E:5B,PROFILE=a2dp' //  add your bluetooth device mac address
+    device: "plughw:0,0" // plugged-in USB card 1, device 0; see aplay -l for a list of playback devices
 }
 
 var player = new soundplayer(options)
