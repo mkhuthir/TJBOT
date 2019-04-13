42f32737c6ec9e25f8175adb24fa250e47f37349
mkhuthir <mkhuthir@gmail.com>
2019-04-13 20:31:10 +0400

    added tone test

--- bash/tone_analyzer_2.sh
+++ bash/tone_analyzer_2.sh
@@ -3,7 +3,7 @@
 # Analyze the contents tone of the file tone.json
 # Using the general-purpose endpoint via the POST request method
 # The file includes a single paragraph of plain text that is written by one person.
-# Issue the following command to analyze the tone of the overall content only by setting the sentences parameter to false.
+# Issue the following command to analyze the tone of the overall content and of each individual sentence.
 
 echo "Analyze the contents tone of the file tone.json"
 
@@ -13,7 +13,7 @@ else
 	echo " Calling Watson Service API Using -> \"apikey:$1\""
 	curl -X POST -u "apikey:$1" \
       --header "Content-Type: application/json" \
-      --data-binary @./json/tone.json \
-      "https://gateway-lon.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21&sentences=false"
+      --data-binary @./json/tone_sentence.json \
+      "https://gateway-lon.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21"
 
 fi
--- bash/tone_analyzer_3.sh
+++ bash/tone_analyzer_3.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# Using the general-purpose endpoint via the GET request method
-# The GET method provides the same functionality and produces the same results as the POST method
-# use the method's text query parameter to specify the content to be analyzed. 
-# GET method accepts only plain text input.
+# Analyze the contents tone of the file tone.json
+# Using the general-purpose endpoint via the POST request method
+# The file includes a single paragraph of plain text that is written by one person.
+# Issue the following command to analyze the tone of the overall content only by setting the sentences parameter to false.
 
 echo "Analyze the contents tone of the file tone.json"
 
@@ -13,6 +13,7 @@ else
 	echo " Calling Watson Service API Using -> \"apikey:$1\""
 	curl -X POST -u "apikey:$1" \
       --header "Content-Type: application/json" \
-      --data-binary @./json/tone_chat.json \
-      "https://gateway-lon.watsonplatform.net/tone-analyzer/api/v3/tone_chat?version=2017-09-21"
+      --data-binary @./json/tone.json \
+      "https://gateway-lon.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21&sentences=false"
+
 fi
