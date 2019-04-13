878f86424039d76b21022d0e75ca92cea2429c54
mkhuthir <mkhuthir@gmail.com>
2019-04-13 20:05:33 +0400

    wip

--- labs/lab7_tone_of_text.js
+++ labs/lab7_tone_of_text.js
@@ -23,12 +23,14 @@ var tj = new TJBot(hardware, tjConfig, credentials);
 
 var text = "Team, I know that times are tough! Product sales have been disappointing for the past three quarters. We have a competitive product, but we need to do a better job of selling it!";
 
+/* Use the following for test
 tj.analyzeTone(text).then(function(response) 
     {
       console.log(response);
     }
   );
-/*
+*/
+
 tj.analyzeTone(text).then(function(response) 
     {
       var emotions = response.document_tone.tone_categories[0].tones;
@@ -42,4 +44,4 @@ tj.analyzeTone(text).then(function(response)
       console.log("Top tone: "+top.tone_id);
     }
   );
-*/
+
